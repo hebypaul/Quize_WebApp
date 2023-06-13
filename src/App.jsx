5f17bf2360af42298e4fa6f995670b6b18a07c19
Heby T Paul <hebytpaul1111@gmail.com>
2023-06-14 02:41:12 +0530

    Fix importing Home issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import QuizPage from './pages/QuizPage';
 
 function App() {
